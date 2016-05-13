1518a0ec0d7898ae7b805a61614bcc4199e4c974
samirkarki <samir.ctec@gmail.com>
2016-05-13 04:09:21 -0600

    shiny app code: slidify- guess the number

---
+++ Slidify Pitch of Guess The Number/codes.R
@@ -0,0 +1,10 @@
+#install and load devtools
+install.packages("devtools")
+library(devtools)
+
+#install and load Slidify
+install_github('slidify', 'samirkarki')
+install_github('slidifyLibraries', 'samirkarki')
+library(slidify)
+
+author("Guess_The_Number")
